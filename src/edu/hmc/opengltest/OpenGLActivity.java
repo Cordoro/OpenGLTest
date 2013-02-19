0d3812fa54f67bfed9339bd5778ff5cdb6f78229
Josef Spjut <josef.spjut@gmail.com>
2013-02-18 19:14:52 -0800

    Added default project.

---
+++ src/edu/hmc/opengltest/OpenGLActivity.java
@@ -0,0 +1,15 @@
+package edu.hmc.opengltest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OpenGLActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
